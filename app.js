d7571ee334add092fa00f9bdd9004b8dcb0b9365
Asim <asimnipty@yahoo.com>
2026-06-06 16:49:14 +0600

    fix: add express-async-errors to dependencies

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 // cPanel Node.js App entry point
 // This tells cPanel where to start the app
-require('./backend/dist/index.js')
+
+
+
+process.chdir(__dirname);
+require('./backend/dist/index.js');
